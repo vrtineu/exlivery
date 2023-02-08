886342c6e558ca7aa36a4a3da32f82dbf2e6c305
vrtineu <vininro@gmail.com>
2023-02-07 22:11:53 -0300

    test: creating item factory

--- test/support/Factory.ex
+++ test/support/Factory.ex
@@ -1,6 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
+  alias Exlivery.Orders.Item
   alias Exlivery.Users.User
 
   def user_factory do
@@ -10,6 +11,15 @@ defmodule Exlivery.Factory do
       email: "john@doe.com",
       cpf: "12345678901",
       age: 18
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza de calabresa",
+      category: :pizza,
+      unity_price: Decimal.new("10.00"),
+      quantity: 1
     }
   end
 end
