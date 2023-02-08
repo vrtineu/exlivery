154ddc91a892a14c1d267035f1e6ef205a2c6f19
vrtineu <vininro@gmail.com>
2023-02-07 22:53:41 -0300

    test: creating order factory

--- test/support/Factory.ex
+++ test/support/Factory.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
 
   def user_factory do
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       category: :pizza,
       unity_price: Decimal.new("10.00"),
       quantity: 1
+    }
+  end
+
+  def order_factory do
+    %Order{
+      user_cpf: "12345678901",
+      delivery_address: "Rua 1",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Hamburger",
+          category: :hamburger,
+          quantity: 2,
+          unity_price: Decimal.new("15.50")
+        )
+      ],
+      total_price: Decimal.new("41.00")
     }
   end
 end
