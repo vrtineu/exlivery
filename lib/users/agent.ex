cc104c9b57acac430d39a97a30c1acd0c4ca5299
vrtineu <vininro@gmail.com>
2023-02-07 23:29:42 -0300

    feat(user): creating user agent

---
+++ lib/users/agent.ex
@@ -0,0 +1,19 @@
+defmodule Exlivery.Users.Agent do
+  use Agent
+  alias Exlivery.Users.User
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%User{} = user), do: Agent.update(__MODULE__, &Map.put(&1, user.cpf, user))
+
+  def get(cpf), do: Agent.get(__MODULE__, &get_user(&1, cpf))
+
+  defp get_user(state, cpf) do
+    case Map.get(state, cpf) do
+      nil -> {:error, :not_found}
+      user -> {:ok, user}
+    end
+  end
+end
