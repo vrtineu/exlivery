d90f14cec76df6a75bd28d87bd2fba04cdc719bd
vrtineu <vininro@gmail.com>
2023-02-08 22:55:23 -0300

    refactor(order): adjusting error return

--- lib/orders/order.ex
+++ lib/orders/order.ex
@@ -17,7 +17,7 @@ defmodule Exlivery.Orders.Order do
      }}
   end
 
-  def build(_user, _items), do: {:error, "Invalid parameters"}
+  def build(_user, _items), do: {:error, :invalid_order}
 
   defp calculate_total_price(items) do
     Enum.reduce(items, Decimal.new("0.00"), &sum_prices(&1, &2))
